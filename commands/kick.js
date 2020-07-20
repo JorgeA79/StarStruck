b1bbc767d30a07074c0b44be126e6b197850b92a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-19 20:43:47 -0500

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: "kick",
+  description: "Pinging the bot",
+  execute(client, message) {
+  
+     if (message.member.hasPermission('KICK_MEMBERS')) {   
+  
+   let member = message.mentions.members.first();
+	    if(!member) 
+	    return message.reply("Try mentioning the person");	
+      
+     member.kick().then((member) => {
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }) 
+
+   }else{
+   return message.reply('You dont have enough permissions for that')
+   }
+   
+}
